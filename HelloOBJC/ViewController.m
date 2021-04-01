774af77741a8264891e5c6a804737ebdc5bdb38d
ayodejiayankola <ayodejiayankola@gmail.com>
2021-04-01 14:01:43 +0100

    objective popup on button click

--- HelloOBJC/ViewController.m
+++ HelloOBJC/ViewController.m
@@ -19,6 +19,13 @@
 }
 
 - (IBAction)clickBait:(id)sender {
+    
+    //initailazer function - which is a static function
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"WELCOME" message:@"Welcome to the world of Objective-c" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *closeAction = [UIAlertAction actionWithTitle:@"CLOSE" style:UIAlertActionStyleCancel handler:nil];
+    [alert addAction:closeAction];
+    //present the alert to view controller
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 
